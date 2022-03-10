e6231af8f4f56e23d009418222b48fd6304cd90c
mistyshanshui <mistyshanshui@outlook.com>
2022-03-10 14:15:59 -0800

    1.11 display statistics in Table

--- part1/feedback exe 1.6-1.14/src/App.js
+++ part1/feedback exe 1.6-1.14/src/App.js
@@ -10,7 +10,10 @@ const Button = (props)=>{
 const StatisticLine = (props) => {
   const {text, value} = props
   return (
-    <p>{text} {value}</p>
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+    </tr>
   )
 }
 
