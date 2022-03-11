62bb4a88310fa28b9c9aab53909823f8d4876971
mistyshanshui <mistyshanshui@outlook.com>
2022-03-11 09:15:38 -0800

    anecdotes 1.12

--- part1/feedback exe 1.6-1.14/src/App.js
+++ part1/feedback exe 1.6-1.14/src/App.js
@@ -45,18 +45,26 @@ const Statistic = (props) => {
 }
 
 const App = () => {
-  // save clicks of each button to its own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+  
+  const anecdotes = [
+    'If it hurts, do it more often',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients'
+  ]
+
+  const [selected, setSelected] = useState(0)
+
+  const randomNumber = ()=>{
+    setSelected(int(Math.random() * anecdotes.length))
+  }
 
   return (
     <div>
-      <h1>give feedback</h1>
-      <Button text={'good'} handler={() => { setGood(good + 1) }} />
-      <Button text={'neutral'} handler={() => { setNeutral(neutral + 1) }} />
-      <Button text={'bad'} handler={() => { setBad(bad + 1) }} />
-      <Statistic good={good} bad={bad} neutral={neutral} />
+      {anecdotes[selected]}
     </div>
   )
 }
