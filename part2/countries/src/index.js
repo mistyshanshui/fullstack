a3c9056d4ef47dd3655df5e4d9c624ebe731b069
mistyshanshui <mistyshanshui@outlook.com>
2022-03-18 16:16:37 -0700

    2.12 Data for countries, step1

---
+++ part2/countries/src/index.js
@@ -0,0 +1,11 @@
+import ReactDOM from 'react-dom';
+import App from './App';
+import axios from 'axios';
+
+axios.get('https://restcountries.com/v3.1/all').then(response => {
+  const countries = response.data
+  ReactDOM.render(
+    <App countries={countries} />,
+    document.getElementById('root')
+  )
+})
