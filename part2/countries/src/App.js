ec9dda69fc9c4d77c8e9e5c4a8adae0037d2a160
mistyshanshui <mistyshanshui@outlook.com>
2022-03-18 16:57:34 -0700

    2.14 to be finished

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,40 +1,53 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
 
-const Countries = ({ countries, filter }) => {
+const Countries = ({ countries, filter, buttonHandler }) => {
   const filtered = countries.filter(c => c.name.common.toLowerCase().includes(filter.toLowerCase()) == true)
-  
+
   if (filtered.length > 10) {
     return (<>Too many matches, specify another filter</>)
   }
-  else if (filtered.length == 1){
+  else if (filtered.length == 1) {
     return (
       <>
-        <h1>{filtered[0].name.common}</h1>
-        <p>capital {filtered[0].capital}</p>
-        <p>area {filtered[0].area}</p>
-        <h2>languages:</h2>
-        <ul>
-          {Object.values(filtered[0].languages).map(language => <li key={language}>{language}</li>)}
-        </ul>
-        <img src={Object.values(filtered[0].flags)[0]} ></img>
+        <CountryView country={filtered[0]}/>
       </>
     )
   }
-  else{
+  else {
+    console.log("nuber of countries are :", filtered.length)
     return (
       <>
-        {filtered.map(c => <p key={c.name.common}>{c.name.common}</p>)}
+        {filtered.map(c => <div>{c.name.common} <button id={c.name.common} onClick={buttonHandler}>show</button></div>)}
       </>
     )
   }
 }
 
-function App({countries}) {
+const CountryView = ({country})=>{
+  return (
+    <>
+      <h1>{country.name.common}</h1>
+      <p>capital {country.capital}</p>
+      <p>area {country.area}</p>
+      <h2>languages:</h2>
+      <ul>
+        {Object.values(country.languages).map(language => <li key={language}>{language}</li>)}
+      </ul>
+      <img src={Object.values(country.flags)[0]} ></img>
+    </>
+  )
+}
+
+function App({ countries }) {
   const [filter, setFilter] = useState('')
 
   const handleFilterChange = (event) => {
     setFilter(event.target.value)
+  }
+
+  const buttonHandler = (event) => {
+    
   }
 
   return (
