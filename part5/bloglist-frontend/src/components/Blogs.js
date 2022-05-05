2963a5657e74be83b9dc21c6082ac2dce425b9f1
mss <mistyshanshui@outlook.com>
2022-05-05 14:38:59 -0700

    5.6 view/hide blog details

--- part5/bloglist-frontend/src/components/Blogs.js
+++ part5/bloglist-frontend/src/components/Blogs.js
@@ -1,8 +1,38 @@
+import { useState } from 'react'
 
-const Blogs = ({ blogs, username}) => {
+const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false)
+
+  const toggleVisibility = () => {
+    setVisible(!visible)
+  }
+
+  const hide = { display: visible ? 'none' : '' }
+  const show = { display: visible ? '' : 'none' }
+  const listStyle = { listStyle: 'none', paddingLeft: 0 }
+  const blogBorder = { border: 'solid', margin: 3 }
+  
+  return (
+    <div key={blog.title} style={blogBorder}>
+      <div style={hide}>
+        {blog.title} <button onClick={toggleVisibility}>view</button>
+      </div>
+      <div style={show}>
+        <li style={listStyle}>{blog.title} <button onClick={toggleVisibility}>hide</button></li>
+        <li style={listStyle}>{blog.url}</li>
+        <li style={listStyle}>likes {blog.likes} <button>like</button></li>
+        <li style={listStyle}>{blog.author}</li>
+      </div>
+    </div>
+  )
+}
+
+const Blogs = ({ blogs }) => {
   return (
     <>
-      {blogs.map(blog => <div key={blog.title}> {blog.title} {blog.author} </div>)}
+      {blogs.map(blog =>
+        <Blog blog={blog} />
+      )}
     </>
   )
 }
